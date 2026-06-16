05fb0106c23415abb6c318eccb4aeaa6d09db0c1
mWaleedh <187419608+mWaleedh@users.noreply.github.com>
2026-06-16 15:08:04 +0500

    first commit

---
+++ tests/basic_adder_test.cpp
@@ -0,0 +1,27 @@
+#include <systemc.h>
+#include "basic_adder.cpp"
+
+int sc_main(int argc, char* argv[]) {
+    // signals
+    sc_signal<sc_uint<4>> num1, num2;
+    sc_signal<sc_uint<5>> sum;
+
+    // instantiate module
+    Adder adder_inst("ADDER_INST");
+        // connect ports
+        adder_inst.num1(num1);
+        adder_inst.num2(num2);
+        adder_inst.sum(sum);
+
+    // Test #1
+    num1.write(5);
+    num2.write(10);
+    sc_start(1, SC_SEC);
+
+    // Test #2
+    num1.write(7);
+    num2.write(15);
+    sc_start(1, SC_SEC);
+
+    return 0;
+}
