34b6db30fd4297f4d730330e3fa8c0eb4dec5eb1
mWaleedh <187419608+mWaleedh@users.noreply.github.com>
2026-06-16 16:01:20 +0500

    update

--- tests/basic_adder_test.cpp
+++ tests/basic_adder_test.cpp
@@ -16,12 +16,12 @@ int sc_main(int argc, char* argv[]) {
     // Test #1
     num1.write(5);
     num2.write(10);
-    sc_start(1, SC_SEC);
+    sc_start(10, SC_NS);
 
     // Test #2
     num1.write(7);
     num2.write(15);
-    sc_start(1, SC_SEC);
+    sc_start(10, SC_NS);
 
     return 0;
 }
