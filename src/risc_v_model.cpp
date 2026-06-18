3b346eb0e4859211f5bfc67a62af0d20d8cc6e7a
mWaleedh <187419608+mWaleedh@users.noreply.github.com>
2026-06-18 15:51:33 +0500

    docs/module_scope.md

--- src/risc_v_model.cpp
+++ src/risc_v_model.cpp
@@ -12,8 +12,8 @@ SC_MODULE(risc_v_model) {
     sc_in<sc_uint<WIDTH>> data_bus_i;
 
     // output ports
-    sc_out<bool> write_en;
-    sc_out<bool> read_en;
+    sc_out<bool> write_en_o;
+    sc_out<bool> read_en_o;
     sc_out<sc_uint<WIDTH>> addr_bus_o;
     sc_out<sc_uint<WIDTH>> data_bus_o;
 
@@ -22,38 +22,57 @@ SC_MODULE(risc_v_model) {
     sc_uint<WIDTH> cur_inst;
     sc_uint<WIDTH> registers[WIDTH];
 
+    // IF: Instruction Fetch
+    // ------------------------------
     void fetch() {
 
     }
 
+    // ID: Instruction Decode
+    // ------------------------------
     void decode() {
 
     }
 
+    // EX: Instruction Execute
+    // ------------------------------
     void execute() {
 
     }
 
+    // MEM: Memory/Peripheral Access
+    // ------------------------------
+    void memoryAccess() {
+
+    }
+
+    // WB: Write Back
+    // ------------------------------
     void writeBack() {
 
     }
 
+    // Main Thread
+    // ------------------------------
     void mainThread() {
+        // reset/initial state logic
         pc = 0;
         cur_inst = 0;
         for (int i = 0; i < WIDTH; i++) {
             registers[i] = 0;
         }
 
-        write_en.write(false);
-        read_en.write(false);
-
         addr_bus_o.write(0);
         data_bus_o.write(0);
 
+        // wait marking end of reset part
         wait();
 
+        // main loop
         while (true) {
+            // reset flags to default before executing each instruction
+            write_en_o.write(false);
+            read_en_o.write(false);
 
         }
     }
@@ -64,6 +83,8 @@ SC_MODULE(risc_v_model) {
     }
 };
 
+// Test Bench
+// ------------------------------
 int sc_main(int argc, char* argv[]) {
 
     return 0;
