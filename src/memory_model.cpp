7e1baf3e784f2f3f34c414dbd7f3b22942374ed9
mWaleedh <187419608+mWaleedh@users.noreply.github.com>
2026-07-31 11:11:13 +0500

    Changed memory to byte-addressable

--- src/memory_model.cpp
+++ src/memory_model.cpp
@@ -26,19 +26,20 @@ SC_MODULE(memory_model) {
     sc_out<sc_uint<WIDTH>> data_bus_o;
 
     // local variables
-    sc_uint<WIDTH> memory[SIZE];
+    sc_uint<8> memory[SIZE];
     sc_uint<WIDTH> max_addr;
 
     // Function to load testbench data
     void load_data(uint32_t addr, uint32_t data) {
-        // Align address
-        addr /= 4;
-
         // Write data to memory if within range
-        if (addr < SIZE) {
+        if (addr + 3 < SIZE) {
             // Make sure it isn't overwriting a previous instruction
             if (addr >= max_addr) {
+                // Write one byte at a time
                 memory[addr] = data;
+                memory[addr + 1] = data >> 8;
+                memory[addr + 2] = data >> 16; 
+                memory[addr + 3] = data >> 24;
             }
             else {
                 cout << "Memory Error: Trying to access already occupied memory 0x" << hex << addr << dec << endl << endl;    
@@ -67,10 +68,15 @@ SC_MODULE(memory_model) {
             // Convert hex string to integer
             uint32_t data = stoul(temp, nullptr, 16);
             
-            if ( max_addr < SIZE) {
+            if ( max_addr + 3 < SIZE) {
+                // Write one byte at a time
                 memory[max_addr] = data;
+                memory[max_addr + 1] = data >> 8;
+                memory[max_addr + 2] = data >> 16; 
+                memory[max_addr + 3] = data >> 24;
+
                 // Move to next address
-                max_addr++;
+                max_addr += 4;
             }
         }
         file.close();
@@ -95,21 +101,24 @@ SC_MODULE(memory_model) {
         // Main loop
         while (true) {
             // Instruction Fetch
-            uint32_t inst_addr = inst_addr_bus_i.read() / 4;
+            uint32_t inst_addr = inst_addr_bus_i.read();
 
             // Check if CPU requested Instruction
             if (inst_read_en_i.read() == true) {
-                if (inst_addr > SIZE - 1) {
+                if (inst_addr + 3 >= SIZE) {
                     inst_bus_o.write(0);
                     cout << "@" << sc_time_stamp() << " Memory Error (Instruction Port): Trying to access invalid memory 0x" << hex << inst_addr_bus_i.read() << dec << endl << endl;
                 }
                 else {
+                    // Combine bytes 
+                    uint32_t data = (memory[inst_addr]) | (memory[inst_addr + 1] << 8) | (memory[inst_addr + 2] << 16) | (memory[inst_addr + 3] << 24);
+
                     // Send Instruction to CPU
-                    inst_bus_o.write(memory[inst_addr]);
+                    inst_bus_o.write(data);
 
                     cout << "@" << sc_time_stamp() << " Memory Read (IF): " << endl;
                     cout << "1. Address -> 0x" << hex << inst_addr_bus_i.read() << endl;
-                    cout << "2. Data -> 0x" << memory[inst_addr] << dec << endl << endl;
+                    cout << "2. Data -> 0x" << data << dec << endl << endl;
                 }
             }
             else {
@@ -117,10 +126,10 @@ SC_MODULE(memory_model) {
             }
 
             // Data Fetch
-            uint32_t data_addr = data_addr_bus_i.read() / 4;
+            uint32_t data_addr = data_addr_bus_i.read();
 
             // If address is out of bounds don't perform any action
-            if (data_addr > SIZE - 1 && (data_read_en_i.read() == true || data_write_en_i.read() == true)) {
+            if (data_addr + 3 >= SIZE && (data_read_en_i.read() == true || data_write_en_i.read() == true)) {
                 // If it is a read operation, write 0 to data_bus
                 if (data_read_en_i.read() == true) {
                     data_bus_o.write(0);
@@ -136,19 +145,26 @@ SC_MODULE(memory_model) {
                 // Give preference to write
                 if (data_write_en_i.read() == true) {
                     // write data to target address
-                    memory[data_addr] = data_bus_i.read();
+                    uint32_t data = data_bus_i.read();
+                    memory[data_addr] = data;
+                    memory[data_addr + 1] = data >> 8;
+                    memory[data_addr + 2] = data >> 16; 
+                    memory[data_addr + 3] = data >> 24;
 
                     cout << "@" << sc_time_stamp() << " Memory Write (MEM): " << endl;
                     cout << "1. Address -> 0x" << hex << data_addr_bus_i.read() << endl;
                     cout << "2. Data -> 0x" << data_bus_i.read() << dec << endl << endl;
                 }
                 else if (data_read_en_i.read() == true) {
+                    // Combine bytes 
+                    uint32_t data = (memory[data_addr]) | (memory[data_addr + 1] << 8) | (memory[data_addr + 2] << 16) | (memory[data_addr + 3] << 24);
+
                     // Read data from target address
-                    data_bus_o.write(memory[data_addr]);
+                    data_bus_o.write(data);
 
                     cout << "@" << sc_time_stamp() << " Memory Read (MEM): " << endl;
                     cout << "1. Address -> 0x" << hex << data_addr_bus_i.read() << endl;
-                    cout << "2. Data -> 0x" << memory[data_addr] << dec << endl << endl;
+                    cout << "2. Data -> 0x" << data << dec << endl << endl;
                 }
                 else {
                     data_bus_o.write(0);
